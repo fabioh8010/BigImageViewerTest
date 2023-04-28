49cb6b5dbe1a8fcfef53b3f994b52d3a64fc7678
Fábio Henriques <contact@fabiohenriques.dev>
2023-04-28 13:21:43 +0100

    Add simple example of BigImageViewer library usage

--- app/src/main/java/com/example/bigimageviewertest/MainActivity.java
+++ app/src/main/java/com/example/bigimageviewertest/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.bigimageviewertest;
 
+import android.net.Uri;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.github.piasy.biv.BigImageViewer;
+import com.github.piasy.biv.loader.glide.GlideImageLoader;
+import com.github.piasy.biv.view.BigImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BigImageViewer.initialize(GlideImageLoader.with(getApplicationContext()));
+
         setContentView(R.layout.activity_main);
+
+        BigImageView bigImageView = (BigImageView) findViewById(R.id.mBigImage);
+        bigImageView.showImage(Uri.parse("file:///android_asset/tall_image.jpeg"));
     }
 }
